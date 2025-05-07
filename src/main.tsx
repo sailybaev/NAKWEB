58e1552c8eada91ca6ab85ac7517795bdc84d802
Sailybaev <sailybaevvv@gmail.com>
2025-05-07 15:25:49 +0500

    build

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './i18n'
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
+            {/**/}
             <App />
         </BrowserRouter>
     </React.StrictMode>
